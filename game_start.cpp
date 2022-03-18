2b477d9364735509a5727e97f9d2811f35541075
AnujinAkul <101851199+AnujinAkul@users.noreply.github.com>
2022-03-18 10:58:17 +0800

    Update game_start.cpp

--- game_start.cpp
+++ game_start.cpp
@@ -1,1 +1,15 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Game Rules\n";
+    cout << "1. 2 players will have 26 cards randomly\n";
+    cout << "2. they will reveal their cards at same time, one by one\n";
+    cout << "3. highest ranking card  will get 1 point\n";
+    cout << "card rank is ace > king > queen > jack > 10 ... etc\n";
+    cout << "4. if 2 players get same ranking card then player who press the space button first will get point\n";
+    cout << "5. if 2 players dont have same card and press space button then player will loose 1 point\n";
+    cout << "6. after 26 rounds player who gopt highest point will be the winner\n";
+    cout << "good luck :)\n";
+    cout << "";
+}
