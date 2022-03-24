77e1f9a3c1d6441c836fb599375060fdc2a0ed7e
Anand0616 <91175530+Anand0616@users.noreply.github.com>
2022-03-24 22:53:19 +0800

    Add files via upload

--- cards.cpp
+++ cards.cpp
@@ -1,1 +1,30 @@
+#include <algorithm>
+#include <random>
+#include <iostream>
+#include <list>
+#include "cards.h"
+using namespace std;
 
+class Card{
+    public:
+    string name;
+    int id;
+    int rank;
+
+};
+
+class Deck{
+    public:
+    list<Card> deck;
+    
+};
+
+class Hand{
+    public:
+    Deck hand;
+};
+//shuffle ni deck awaad deck butsaaj ugnu
+Deck shuffle(Deck deck){
+ random_shuffle(deck.begin(), deck.end());
+
+}
